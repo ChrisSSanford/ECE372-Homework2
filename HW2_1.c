dc335e45c8069470e0d2663bd59dc50421d1b92f
Christopher Houseman <chouseman@email.arizona.edu>
2014-10-08 14:40:00 -0700

    created some changes to the code so that it looks better and may be closer to working

--- HW2_1.c
+++ HW2_1.c
@@ -1,5 +1,11 @@
+#include "p24fj64ga002.h"
+#include <stdio.h>
+
+typedef enum stateTypeEnum{WaitForPress, WaitForRelease, LEDToggle} stateType; //create a struct of type stateType to be used in switch statement
+
 int main(void)
 {
+     stateType state;  //creates a state variable to be used in the switch statement
 
     while(1)
     {
@@ -8,17 +14,19 @@ int main(void)
                     if(PORTBbits.RB5 == 0) // when SW1 is pressed
                     {
                         state = 0;
-                        //PR1 = 14400/2; //doubled the blinking rate of the active LED
                         TMR1 = 0; // resets the TMR1 register to 0, EXTRA CREDIT: if this is not done then I noticed a delay while pressing SW1 before the LED changed blinking speed
 
                     }
                     break;
 
                 case 0:
-                    //LATB = LATB^0x8000;
-                    print that it is in state 0;
-                    if SW1 is held for 5 seconds or more move to state 1;
-                    if anything else stay in this state;
+                    
+                    printf ("State 0");
+                    T1CON.TON = 1;
+                    if(TMR1 == 5 && PORTBbits.RB5 == 0)
+                    state = 1;
+                    else
+                        state = 0;
                     //state = WaitForRelease;
                     break;
 
@@ -30,7 +38,7 @@ int main(void)
 //                        TMR1 = 0; // resets the TMR1 register to 0
 //                    }
 
-                    print that it is in state 1;
+                    printf("State 1");
                     if SW1 is pressed 1 time (meaning that another press is not detected within .25 s of the first press) move to state 2;
                     if SW1 is pressed 2 times (meaning that another press is detected within .25 s of the first press) move to state 3;
                     if SW1 is held for 5 seconds or more move back to state 0;
@@ -38,13 +46,13 @@ int main(void)
                     break;
 
                 case 2:
-                    print that it is in state 2;
+                    printf("State 2");
                     start timer;
                     if in this state for 5ms move back to state 1;
                     break;
 
                 case 3:
-                    print that it is in state 3;
+                    printf("State 3");
                     start timer;
                     if in this state for 5ms move back to state 2;
                     break;
